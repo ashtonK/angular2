9c8eac0cdc46503d110e0a8a1b072224ec0714ef
ashtonK <ashtonk92@gmail.com>
2016-03-31 16:35:33 -0400

    Added admin functionality for testing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,47 @@
+/*
+Reference the elements that we need.
+Angular apps are modular, many files each dedicate to purpose.
+Component Decorator function from main Angular library module
+*/
 import {Component} from 'angular2/core';
 
+/*
+
+Tells Angular what template to use and how to create component.
+Component decorator function takes a metadataobject.
+*/
+
+
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+	selector: 'main-navbar',
+    templateUrl: 'templates/navbar.html'
 })
-export class AppComponent { }
+export class MainNav { 
+	public title ='Navbar';
+
+} 
+@Component({
+	selector: 'app-body',
+    templateUrl: 'templates/body.html'
+})
+export class LoginScreen {
+	status = 'login';
+	adminPage = 'candidates';
+
+	adminPageSwitch(page){
+		console.log(page+" is the new admin page");
+		this.adminPage=page;
+	}
+	adminLogin(){
+		console.log("Admin Mode");
+		this.status='admin';
+	}
+	nextpage(){
+		if(this.status==='login'){
+			this.status='agreement';
+		} else if(this.status ==='agreement'){
+			this.status='instruction';
+		}
+	}
+
+}
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,23 @@
-import {bootstrap}    from 'angular2/platform/browser';
-import {AppComponent} from './app.component';
 
-bootstrap(AppComponent);
+/*
+This main.ts file could easily be in app.components and is tiny
+this is here for proper structure.
+
+*/
+import {bootstrap} from 'angular2/platform/browser';
+/*
+Import Angular's brower bootstrap function
+imported from browser, not core. no single way to bootstrap app.
+*/
+
+/*
+Import Application root component
+*/
+import {LoginScreen} from './app.component';
+import {MainNav} from './app.component';
+
+bootstrap(LoginScreen);
+bootstrap(MainNav);
+/*
+Call Bootstrap with AppComponent.
+*/
